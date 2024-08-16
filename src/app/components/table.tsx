715c2462e54ea58e1b168f1dd5ed1ea0535cda50
JuanAmezquitaAgredo <jagredo03@gmail.com>
2024-08-16 10:32:06 -0500

    JuanAmezquita_Add_DeleteFuntion

--- src/app/components/table.tsx
+++ src/app/components/table.tsx
@@ -4,7 +4,7 @@ import { Product } from '../models/table.model';
 import { TableContainer, TableElement, TableHeader, TableCell, TableRow, Button } from './styles/styled-components';
 import Popup from './popup';
 import SimpleForm from './simpleForm';
-import { AlertConfirm, AlertMessage } from '../utils/alert';
+import { handleClosePopup, handleDelete, handleEditClick, handleSubmitEdit } from '../services/ProductController';
 
 export default function Table() {
     const [products, setProducts] = useState<Product[]>([]);
@@ -28,16 +28,6 @@ export default function Table() {
         fetchData();
     }, []);
 
-    const handleEditClick = (product: Product) => {
-        setSelectedProduct(product);
-        setIsPopupOpen(true);
-    };
-
-    const handleClosePopup = () => {
-        setIsPopupOpen(false);
-        setSelectedProduct(null);
-    };
-
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { id, value } = e.currentTarget;
         setSelectedProduct({
@@ -45,44 +35,6 @@ export default function Table() {
             [id]: id === 'price' ? parseFloat(value) : value,
         });
     };
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (selectedProduct) {
-            try {
-                // Mostrar el alerta de confirmación
-                const result = await AlertConfirm('Are you sure you want to update this product?');
-    
-                // Verificar si el usuario confirmó la acción
-                if (result.isConfirmed) {
-                    const response = await fetch(`http://localhost:3001/products/${selectedProduct.id}`, {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify(selectedProduct),
-                    });
-    
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    
-                    await AlertMessage('Successfully updated product', 'success');
-                    const updatedProduct = await response.json();
-                    setProducts(products.map(product =>
-                        product.id === updatedProduct.id ? updatedProduct : product
-                    ));
-                    handleClosePopup();
-                    window.location.reload(); // Recarga la página después de guardar
-                }
-            } catch (error) {
-                console.error('Error updating product:', error);
-                await AlertConfirm('Error updating product'); // Mostrar un alerta de error si falla
-            }
-        }
-    };
-    
-    
 
     return (
         <>
@@ -115,8 +67,8 @@ export default function Table() {
                                     )}
                                 </TableCell>
                                 <TableCell>
-                                    <Button onClick={() => handleEditClick(product)}>Edit</Button>
-                                    <Button>Delete</Button>
+                                    <Button onClick={() => handleEditClick(product, setSelectedProduct, setIsPopupOpen)}>Edit</Button>
+                                    <Button onClick={() => handleDelete(products, setProducts, product, () => handleClosePopup(setIsPopupOpen, setSelectedProduct))}>Delete</Button>
                                 </TableCell>
                             </TableRow>
                         ))}
@@ -125,9 +77,9 @@ export default function Table() {
             </TableContainer>
 
             {selectedProduct && (
-                <Popup isOpen={isPopupOpen} onClose={handleClosePopup}>
+                <Popup isOpen={isPopupOpen} onClose={() => handleClosePopup(setIsPopupOpen, setSelectedProduct)}>
                     <SimpleForm
-                        onSubmit={handleSubmit}
+                        onSubmit={(e) => handleSubmitEdit(e, products, setProducts, selectedProduct, () => handleClosePopup(setIsPopupOpen, setSelectedProduct))}
                         handleChange={handleChange}
                         title={selectedProduct.title}
                         description={selectedProduct.description}
