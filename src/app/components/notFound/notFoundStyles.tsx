6b93c77d6d60a22ca784f2739fa8fd767a8bf8cc
Karina P <karina_pineda97@hotmail.com>
2024-08-18 21:18:01 -0500

    feature component NotFound

---
+++ src/app/components/notFound/notFoundStyles.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+  background-color: #F9F9F9;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Message = styled.p`
+  font-size: 1.25rem;
+  margin-bottom: 1rem;
+  font-weight: bold;
+`;
+
+export const Image = styled.img`
+  width: 300px;
+  height: 300px;
+`;
